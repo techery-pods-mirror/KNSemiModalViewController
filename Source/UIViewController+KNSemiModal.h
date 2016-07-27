cbab27eb04d73b1564a92b005c245eb80b2340a5
Eugene Tulusha <tulusheg@gmail.com>
2016-07-27 18:10:00 +0300

    Fix naming issues

--- Source/UIViewController+KNSemiModal.h
+++ Source/UIViewController+KNSemiModal.h
@@ -3,6 +3,9 @@
 //  Created by YangMeyer on 08.10.12.
 //  Copyright (c) 2012 Yang Meyer. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+
 @interface NSObject (YMOptionsAndDefaults)
 - (void)ym_registerOptions:(NSDictionary *)options
 				  defaults:(NSDictionary *)defaults;
